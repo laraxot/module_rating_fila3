1b0b8f12653764356a5530742084966a5882a585
marco76tv <marco.sottana@gmail.com>
2022-06-14 14:27:10 +0200

    .

--- Models/Traits/HasLikes.php
+++ Models/Traits/HasLikes.php
@@ -46,18 +46,13 @@ trait HasLikes {
      * @return void
      */
     public function dislikedBy(User $user) {
-<<<<<<< HEAD
         /**
          * @var \Modules\Rating\Models\Like
          */
         $where = $this->likesRelation()->where('user_id', $user->id)->first();
-        $where->delete();
-=======
-        $where = $this->likesRelation()->where('user_id', $user->id)->first();
         if (null != $where) {
             $where->delete();
         }
->>>>>>> a13db4c (.)
 
         $this->unsetRelation('likesRelation');
     }
