362f24c309673908dd83e7e6577d9bab0b2ff917
marco76tv <marco76tv@users.noreply.github.com>
2023-02-06 11:00:08 +0000

    Fix styling

--- Providers/RatingServiceProvider.php
+++ Providers/RatingServiceProvider.php
@@ -6,8 +6,7 @@ namespace Modules\Rating\Providers;
 
 use Modules\Xot\Providers\XotBaseServiceProvider;
 
-class RatingServiceProvider extends XotBaseServiceProvider
-{
+class RatingServiceProvider extends XotBaseServiceProvider {
     protected string $module_dir = __DIR__;
     protected string $module_ns = __NAMESPACE__;
     public string $module_name = 'rating';
--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -6,8 +6,7 @@ namespace Modules\Rating\Providers;
 
 use Modules\Xot\Providers\XotBaseRouteServiceProvider;
 
-class RouteServiceProvider extends XotBaseRouteServiceProvider
-{
+class RouteServiceProvider extends XotBaseRouteServiceProvider {
     protected string $moduleNamespace = 'Modules\Rating\Http\Controllers';
 
     protected string $module_dir = __DIR__;
