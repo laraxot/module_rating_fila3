c91270323d369b02e402adeef95f4a9167a24970
marco76tv <marco76tv@users.noreply.github.com>
2023-03-29 08:19:38 +0000

    Fix styling

--- Models/Like.php
+++ Models/Like.php
@@ -10,12 +10,13 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Modules\Rating\Models\Like.
  *
- * @property int $id
- * @property int $user_id
- * @property string|null $likeable_type
- * @property int|null $likeable_id
+ * @property int                             $id
+ * @property int                             $user_id
+ * @property string|null                     $likeable_type
+ * @property int|null                        $likeable_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|Like newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Like newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Like query()
@@ -25,6 +26,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Like whereLikeableType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Like whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Like whereUserId($value)
+ *
  * @mixin \Eloquent
  */
 final class Like extends Model {
