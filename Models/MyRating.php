79ff3c225e328ac320d5109ea2408e27ff5b58de
marco76tv <marco.sottana@gmail.com>
2023-03-29 10:18:19 +0200

    up

--- Models/MyRating.php
+++ Models/MyRating.php
@@ -7,46 +7,38 @@ namespace Modules\Rating\Models;
 /**
  * Modules\Rating\Models\MyRating.
  *
- * @property int                                                                  $post_id
- * @property string|null                                                          $related_type
- * @property string|null                                                          $created_by
- * @property string|null                                                          $updated_by
- * @property string|null                                                          $deleted_by
- * @property \Illuminate\Support\Carbon|null                                      $created_at
- * @property \Illuminate\Support\Carbon|null                                      $updated_at
- * @property string|null                                                          $guid
- * @property string|null                                                          $image_src
- * @property string|null                                                          $lang
- * @property string|null                                                          $post_type
- * @property string|null                                                          $subtitle
- * @property string|null                                                          $title
- * @property string|null                                                          $txt
- * @property string|null                                                          $user_handle
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Image[] $images
- * @property int|null                                                             $images_count
- * @property \Modules\Lang\Models\Post|null                                       $post
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
- * @property int|null                                                             $posts_count
- * @property mixed                                                                $url
- *
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      newQuery()
+ * @property int $post_id
+ * @property string|null $related_type
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property string|null $deleted_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $guid
+ * @property string|null $image_src
+ * @property-read string|null $lang
+ * @property-read string|null $post_type
+ * @property string|null $subtitle
+ * @property string|null $title
+ * @property string|null $txt
+ * @property-read string|null $user_handle
+ * @property-read \Modules\Lang\Models\Post|null $post
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Modules\Lang\Models\Post> $posts
+ * @property-read int|null $posts_count
+ * @property-write mixed $url
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang ofItem(string $guid)
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      query()
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      wherePostId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      whereRelatedType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating      whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating query()
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating whereRelatedType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MyRating whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang withPost(string $guid)
- *
  * @mixin \Eloquent
- *
- * @property int $id
- *
- * @method static \Illuminate\Database\Eloquent\Builder|MyRating whereId($value)
  */
 class MyRating extends BaseModelLang {
     /**
