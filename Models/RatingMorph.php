f3f4da41b03de10775aedd54a7aece653d59f2e2
marco xot <marco.sottana@gmail.com>
2023-03-29 11:05:10 +0200

    up

--- Models/RatingMorph.php
+++ Models/RatingMorph.php
@@ -7,34 +7,33 @@ namespace Modules\Rating\Models;
 /**
  * Modules\Rating\Models\RatingMorph.
  *
- * @property int                                $id
- * @property string|null                        $post_type
- * @property int|null                           $post_id
- * @property int|null                           $rating_id
- * @property int|null                           $value
- * @property string|null                        $created_by
- * @property string|null                        $updated_by
- * @property string|null                        $deleted_by
- * @property \Illuminate\Support\Carbon|null    $created_at
- * @property \Illuminate\Support\Carbon|null    $updated_at
- * @property int|null                           $user_id
+ * @property int $id
+ * @property string|null $post_type
+ * @property int|null $post_id
+ * @property string|null $related_type
+ * @property int|null $related_id
  * @property \Modules\Rating\Models\Rating|null $rating
- *
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property string|null $deleted_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null $auth_user_id
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph query()
+ * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereAuthUserId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereCreatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereDeletedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph wherePostId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph wherePostType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereRatingId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereRelatedId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereRelatedType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereUserId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|RatingMorph whereValue($value)
- *
  * @mixin \Eloquent
  */
 class RatingMorph extends BaseMorphPivot {
