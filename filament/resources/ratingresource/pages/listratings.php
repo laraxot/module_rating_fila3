34485319a5f93695dff3908cde27f14debacdbc7
Retype GitHub Action <retype@object.net>
2024-11-13 16:17:56 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- filament/resources/ratingresource/pages/listratings.php
+++ filament/resources/ratingresource/pages/listratings.php
@@ -4,18 +4,112 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Filament\Resources\RatingResource\Pages;
 
-use Filament\Pages\Actions;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\ActionsPosition;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Table;
 use Modules\Rating\Filament\Resources\RatingResource;
+use Modules\UI\Enums\TableLayoutEnum;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
+use Modules\Xot\Filament\Traits\TransTrait;
 
 class ListRatings extends ListRecords
 {
+    use TransTrait;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
+
     protected static string $resource = RatingResource::class;
 
-    protected function getActions(): array
+    protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make(),
         ];
+    }
+
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            TableLayoutToggleTableAction::make(),
+        ];
+    }
+
+    public function getGridTableColumns(): array
+    {
+        return [
+            Stack::make($this->getListTableColumns()),
+        ];
+    }
+
+    public function getListTableColumns(): array
+    {
+        return [
+            TextColumn::make('id')->sortable()->searchable(),
+            // TextColumn::make('extra_attributes.type')->label('type'),
+            // TextColumn::make('extra_attributes.anno')->label('anno'),
+            TextColumn::make('title')->sortable()->searchable(),
+            TextColumn::make('rule')->badge(),
+            // TextColumn::make('is_readonly'),
+            // TextColumn::make('is_disabled'),
+            // ToggleColumn::make('is_readonly'),
+            IconColumn::make('is_disabled')->boolean(),
+            IconColumn::make('is_readonly')->boolean(),
+            // TextColumn::make('color'),
+        ];
+    }
+
+    public function getTableFilters(): array
+    {
+        return [
+        ];
+    }
+
+    public function getTableActions(): array
+    {
+        return [
+            ViewAction::make()
+                ->label(''),
+            EditAction::make()
+                ->label(''),
+            DeleteAction::make()
+                ->label('')
+                ->requiresConfirmation(),
+        ];
+    }
+
+    public function getTableBulkActions(): array
+    {
+        return [
+            DeleteBulkAction::make(),
+        ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            // ->columns($this->getTableColumns())
+            ->columns($this->layoutView->getTableColumns())
+            ->contentGrid($this->layoutView->getTableContentGrid())
+            ->headerActions($this->getTableHeaderActions())
+
+            ->filters($this->getTableFilters())
+            ->filtersLayout(FiltersLayout::AboveContent)
+            ->persistFiltersInSession()
+            ->actions($this->getTableActions())
+            ->bulkActions($this->getTableBulkActions())
+            ->actionsPosition(ActionsPosition::BeforeColumns)
+            ->defaultSort(
+                column: 'created_at',
+                direction: 'DESC',
+            );
     }
 }
